7c09e9cf1ed5615c89b800a69170dc062ac37e13
WarForgedFolio <gontasouda@gmail.com>
2025-09-28 11:18:32 -0400

    update 2

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,6 +3,8 @@ extends Node
 var score = 0
 @onready var win_label: Label = $"../Player2/Camera2D/Win_Label"
 
+@onready var timer: Timer = $Timer
+
 @onready var score_label: Label = $ScoreLabel
 
 func add_point():
@@ -10,6 +12,7 @@ func add_point():
 	score_label.text = "You collected " + str(score) + " disks."
 	if check_win() == true:
 		win_label.text = "You Win!"
+		timer.start()
 
 func check_win():
 	if score == 3:
@@ -20,3 +23,7 @@ func check_win():
 func end_game():
 	return null
 	
+
+
+func _on_timer_timeout() -> void:
+	get_tree().quit()
